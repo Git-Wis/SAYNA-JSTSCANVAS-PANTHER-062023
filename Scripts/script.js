5c94238c6f0e8390bc0c4cc3ea4458a680f5e796
MILIA Juvaldo Wisman <juvaldo.milia119@gmail.com>
2023-07-10 12:13:17 +0300

    ajout de l'animation canvas

--- Scripts/script.js
+++ Scripts/script.js
@@ -33,77 +33,20 @@ function fadeLeft(){
 
 });
 
-//section slide 
 
-const next = document.querySelector('.next')
-const slide = document.querySelectorAll('.slide')
-let curentSlide = 0
-function startSlide(){
-    let NbSlide
-    //verifiaction la limite du tableau de slide 
-    if(curentSlide < 0){
-        curentSlide = (slide.length - 1)
-    }else if(curentSlide >= slide.length){
-        curentSlide = 0
-    }
+//animation canva
 
-    slide[curentSlide].classList.remove("active")
-    slide[curentSlide].classList.remove("selected")
-    slide[curentSlide+1].classList.add('selected')
-    slide[curentSlide + 2].classList.remove('slide0')
-    slide[curentSlide + 3].classList.add('active')
-    slide[curentSlide + 3].classList.add('slide0')
-
-    curentSlide++
-    console.log(curentSlide)
-
-    
-}
-
-next.addEventListener('click',startSlide)
-
-//animation de l'hover d'un image
-const hoverImgElement = document.querySelector(".imgHoverStart")
-
-function hoveringImg(element){
-    element.classList.add('img-hover')
-}
-
-function hoveringImg_out(element){
-    element.classList.remove('img-hover')
-}
-
-document.querySelector(".row-card").addEventListener('mouseover',()=>{
-    console.log("hover")
-    hoveringImg(hoverImgElement)
-})
-document.querySelector(".row-card").addEventListener('mouseout',()=>{
-    console.log("hover-out")
-    hoveringImg_out(hoverImgElement)
-})
-
-//section formulaire
+const cursor = document.querySelector(".cursor")
+document.addEventListener("mousemove", function (e) {
+  let cursorWidth = cursor.offsetWidth
+  let x = e.clientX - (cursorWidth / 2)
+  let y = e.clientY - (cursorWidth / 2)
+  cursor.style.left = `${x}px`
+  cursor.style.top = `${y}px`
 
 
-const formBtn = document.querySelector('.btnInput')
-const Popup = document.querySelector('.container-popup')
+});
 
-formBtn.addEventListener('click',()=>{
-    let inputNom = document.querySelector('.nom').value
-    let inputEmail = document.querySelector('.email').value
-    let inputMess = document.querySelector('textarea').value
 
-    if(inputNom.trim() === "" || inputEmail.trim() === "" || inputMess.trim() === ""){
-        console.log("Null")
-    }else{
-        //dans le cas ou tout a ete remplit 
-        Popup.style.display = "flex"
-        document.querySelector('form').reset()
-    }
-    console.log('test')
-})
 
-document.querySelector('.popup-btn .btn').addEventListener('click',()=>{
-    Popup.style.display = "none"
-})
 
